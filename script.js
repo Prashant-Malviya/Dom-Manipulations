aedce7c4ac21c414a91dbf76dfc616e55086d24a
Prashant Malviya <prashantmalviya272002@gmail.com>
2023-09-07 15:49:33 +0530

    files added

---
+++ script.js
@@ -0,0 +1,16 @@
+// 1. Selection Of an Element 
+// 2. Changing <HTML>
+// 3. Changing CSS
+// 4. Event Listner 
+
+
+
+// document.querySelector("h1").innerHTML = "hi"
+var a = document.querySelector("h1");
+console.log(a);
+
+a.innerHTML = "hiiiiiiiiiiiiii";
+
+var b = document.querySelector("h1");
+b.style.color = "yellow"
+a.style.backgroundColor = "black"
