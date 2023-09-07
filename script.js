017a0a8d766949eded56a87cfb7adad574f78a92
Prashant Malviya <prashantmalviya272002@gmail.com>
2023-09-07 16:34:17 +0530

    bulb on off program added

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ a.innerHTML = "hiiiiiiiiiiiiii";
 var b = document.querySelector("h1");
 b.style.color = "yellow"
 a.style.backgroundColor = "black"
+
+a.addEventListener("click",function(){
+    a.innerHTML = "Namaste Bharat";
+    console.log("bharat");
+    a.style.color = 'blue';
+})
+
+var bulb = document.querySelector("#bulb");
+var btn = document.querySelector("button");
+
+var flag = 0;
+
+btn.addEventListener("click",function(){
+    if(flag == 0){
+    bulb.style.backgroundColor = "yellow";
+    flag = 1;
+    }else{
+        bulb.style.backgroundColor = "white";
+        flag = 0;
+    }
+})
